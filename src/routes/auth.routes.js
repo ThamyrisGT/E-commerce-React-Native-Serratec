5c65575d1a528289e9cfcb16dcc10bbf9be2e551
Thamyris <tgthurler@gmail.com>
2021-07-13 15:59:24 -0300

    Finalizando as navegações

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native'
-//import { StackNavigator } from '../../navigation/stackNavigation';
-//import BottomTabNavigator from "../../navigation/tabNavigator";
-import DNavigator from '../../navigation/drawerNavigation';
+import BottomTabNavigator from "../../navigation/tabNavigator";
 
-// import Login from '../screens/Login';
-// import Register from '../screens/Register';
-// import Cart from '../screens/Cart';
-// import Categories from '../screens/Categories';
 
 const Routes = () => {
     return (
       <NavigationContainer>
-      <DNavigator/>
+      <BottomTabNavigator/>
       </NavigationContainer>
     );
   };
