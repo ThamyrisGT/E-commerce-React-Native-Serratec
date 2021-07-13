89a019afeb7f7c677be87eb087db9bf574143c40
Thamyris <tgthurler@gmail.com>
2021-07-13 16:52:20 -0300

    Refatorando

--- navigation/tabNavigator.js
+++ navigation/tabNavigator.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StackNavigator, FooterNavigator } from './stackNavigation';
+import { StackNavigator} from './stackNavigation';
 import Cart from "../src/screens/Cart";
-import More from "../src/screens/More";
 import Categories from '../src/screens/Categories';
 
 
@@ -14,7 +13,6 @@ const BottomTabNavigator = () => {
       <Tab.Screen name="Home" component={StackNavigator} />
       <Tab.Screen name="Categories" component={Categories} />
       <Tab.Screen name="Cart" component={Cart} />
-      {/* <Tab.Screen name="More" component={More} /> */}
     </Tab.Navigator>
   );
 };
