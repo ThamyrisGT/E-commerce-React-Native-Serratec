7b356166a84275d2ae012fb530e6641c8a04b70a
alec2 <aleccanto@gmail.com>
2021-07-13 15:45:34 -0300

    adicionando navegação em páginas

--- navigation/tabNavigator.js
+++ navigation/tabNavigator.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {StackNavigator,FooterNavigator } from './stackNavigation';
+import { StackNavigator, FooterNavigator } from './stackNavigation';
+import Cart from "../src/screens/Cart";
+import More from "../src/screens/More";
+import Categories from '../src/screens/Categories';
 
 
 const Tab = createBottomTabNavigator();
@@ -9,9 +12,9 @@ const BottomTabNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={StackNavigator} />
-      <Tab.Screen name="Categories" component={FooterNavigator} />
-      <Tab.Screen name="Cart" component={FooterNavigator} />
-      <Tab.Screen name="More" component={FooterNavigator} />
+      <Tab.Screen name="Categories" component={Categories} />
+      <Tab.Screen name="Cart" component={Cart} />
+      {/* <Tab.Screen name="More" component={More} /> */}
     </Tab.Navigator>
   );
 };
