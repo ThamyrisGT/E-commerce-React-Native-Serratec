406c9acccc28de5635dcab06ee31b6e75aa6e998
andradeClayton <sussatere@gmail.com>
2021-07-14 15:00:21 -0300

    atualização detalhe produto

--- src/screens/ProductDetails/index.js
+++ src/screens/ProductDetails/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text,Image} from 'react-native';
 import {styles} from './styles';
 import Button from '../../components/Button';
-
-const ProductDetails = ({navigation}) => {
+import BoxProduct from '../../components/boxProduct'
+const ProductDetails = ({navigation},props) => {
   return (
-    <View style={styles.container}>
-      <Text> Detalhes do Pedido </Text>
-      <Button title="Adicionar ao Carrinho" activeOpacity={0.7} cadastrar={() => navigation.navigate('Cart')}/>
+    <View style={styles.container,{backgroundColor:'#d3d3d3'}}>
+      {/* <Text> Detalhes do Pedido </Text>
+      <Button title="Adicionar ao Carrinho" activeOpacity={0.7} cadastrar={() => navigation.navigate('Cart')}/> */}
+      <View>
+
+        {/* <BoxProduct/> */}
+
+          <View style={styles.conatainerPrincipal}>
+
+              <View style={styles.viewSuperior}>
+                  <Image source={require('../../assets/images/aquaplay.jpg')} resizeMode='contain' />
+                  <View style={styles.viewSuperiorComprar}>
+                    <Text style={styles.textoProduto}>{props.nome}produto</Text>
+                    <Text style={styles.textoPreco}>{props.preco}preço</Text>
+                  
+                  </View>
+
+              </View>
+              <View style={styles.viewInferior}>
+                  <View style={styles.linhaDivisoria}/>
+                  <View style={{flex:5}}>
+                     <Text style={styles.textoDescricao}>{props.descricao}decricao do produto</Text>
+                  </View>
+                  <View style={{flex:2,justifyContent:'flex-start',alignItems:'center'}}>
+                    
+                       <Button  title="Adicionar ao Carrinho" activeOpacity={0.7} cadastrar={() => navigation.navigate('Cart')}/>
+                    
+                  </View>
+                 
+
+                
+              </View>
+
+
+          </View>
+        
+      </View>
+    
     </View>
   );
 };
--- src/screens/ProductDetails/styles.js
+++ src/screens/ProductDetails/styles.js
@@ -5,4 +5,58 @@ export const styles = StyleSheet.create({
   container: {
    flex:1
   },
+
+  conatainerPrincipal:{
+    height:"90%",
+    width:'90%',
+    flexDirection:'column',
+    alignSelf:'center',
+    marginTop:20
+  },
+  viewSuperior:{
+      flex:3,
+      backgroundColor:'#fff',
+      flexDirection:'row',
+      justifyContent:'space-between'
+  },
+  viewSuperiorComprar:{
+      flex:1,
+      flexDirection:'column',
+      justifyContent:'space-around',
+      alignItems:'center',
+      backgroundColor:'#fff'
+  },
+  textoProduto:{
+      fontSize:25,
+      marginTop:0,
+  },
+  textoPreco:{
+      fontSize:20,
+      marginTop:10
+  },
+  viewInferior:{
+      flex:5,
+      backgroundColor:'#fff',
+      // flexDirection:'column',
+      justifyContent:'flex-start',
+ 
+
+      
+  },
+  linhaDivisoria:{
+      borderTopWidth:2,
+      borderTopColor:'#8a2be2',
+      width:'95%',
+      alignSelf:'center'
+  },
+  textoDescricao:{
+      height:'90%',
+      backgroundColor:'#fff',
+      fontSize:20,
+      textAlign:'justify',
+      marginTop:10,
+      marginHorizontal:15
+  },
+
+
 });
