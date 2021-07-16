9b3a032e21dc2b082436aa4b6ca7e2978b332cce
alec2 <aleccanto@gmail.com>
2021-07-16 10:29:58 -0300

    reajustando tela de pagamento

--- src/screens/Payments/index.js
+++ src/screens/Payments/index.js
@@ -1,44 +1,44 @@
 import React from 'react';
-import {View, Text, Image,Alert} from 'react-native';
-import {styles} from './styles';
+import { View, Text, Image, Alert } from 'react-native';
+import { styles } from './styles';
 import Button from '../../components/Button';
 import Header from '../../components/header';
 import Input from '../../components/input';
 
 
-const Payments = ({navigation}) => {
+const Payments = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Header
-          text={'Voltar'}
-          screen={'Pagamento'}
-          voltar={() => navigation.navigate('Cart')} />
+        text={'Voltar'}
+        screen={'Pagamento'}
+        voltar={() => navigation.navigate('Cart')} />
 
 
       <Text style={styles.texto}>Formas de Pagamento </Text>
       <View style={styles.containerImg} >
-        <Image 
-        style={styles.img} 
-        source={require('../../assets/pagamento.png')}
+        <Image
+          style={styles.img}
+          source={require('../../assets/pagamento.png')}
         />
       </View>
       <View style={styles.inputPay}>
         <View style={styles.inputPay}>
-          <Input textContentType={'text'} placeholder={'Número do cartão'} />
+          <Input keyboardType="numeric" placeholder={'Número do cartão'} />
         </View>
         <View style={styles.inputPay}>
-          <Input textContentType={'text'} placeholder={'Nome'} /> 
+          <Input placeholder={'Nome'} />
         </View >
         <View style={styles.inputPay}>
-           <Input textContentType={'text'} placeholder={'Validade do cartão'} /> 
+          <Input keyboardType="numeric" placeholder={'Validade do cartão'} />
         </View>
         <View style={styles.inputPay}>
-          <Input textContentType={'password'} placeholder={'Senha do cartão'} />
+          <Input ecureTextEntry={true} placeholder={'Senha do cartão'} />
         </View>
       </View>
-      <Button title="Confirmar pagamento" 
-      activeOpacity={0.7} 
-      continuar={() => Alert.alert("Pedido feito com sucesso!", "Agradecemos a preferência ;)")}/>
+      <Button title="Confirmar pagamento"
+        activeOpacity={0.7}
+        continuar={() => Alert.alert("Pedido feito com sucesso!", "Agradecemos a preferência ;)")} />
     </View>
   );
 };
